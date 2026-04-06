b1ed4c6aa309e003af8f03bcf6ca5e43739f1f80
FocusedLoop <joshuawlod2003@gmail.com>
2026-04-06 18:56:59 +1000

    Added avoid obstacles

--- src/entities.py
+++ src/entities.py
@@ -27,33 +27,34 @@ class enemy(entity, entityController):
         entityController.__init__(self, world, self)
 
         self.state = [
-            "TOWARDS_PLAYERS",
-            #"RANDOM_MOVE"
-            #"OBSTACLE_AVOIDANCE"
+            ("TOWARDS_PLAYERS", 0.65),
+            ("OBSTACLE_AVOIDANCE", 0.2),
+            ("RANDOM_MOVE", 0.15)
+            
         ] + state
+        self.last_direction = None
         self.possible_actions = [
             "MOVE_UP",
             "MOVE_DOWN",
             "MOVE_LEFT",
             "MOVE_RIGHT",
         ] + actions
-        
-        # self.starting_position(world)
     
     # State the actions of the entity
     # TODO: IMRPOVE AI CHOICE MAKING, SPERATE CLASS WITH STACK OF ACTIONS AND PROBABILITIES
     def brain(self):
-        chosen = rand.choice(self.state)
+        states, weights = zip(*self.state)
+        chosen = rand.choices(states, weights)[0]
         if chosen == "TOWARDS_PLAYERS":
-            self.move_towards_player(self.world)
+            self.move_towards_player()
         elif chosen == "RANDOM_MOVE":
-            self.random_move(self.world)
+            self.random_move()
         elif chosen == "OBSTACLE_AVOIDANCE":
-            self.avoid_obstacles(self.world)
+            self.avoid_obstacles()
     
-    # TDOD: FINISH
-    def move_towards_player(self, world):
-        player_positions = world.findPlayersPosition()
+    # Move towards the player by comparing the enemy's position to the player's position and moving in the direction that reduces the distance
+    def move_towards_player(self):
+        player_positions = self.world.findPlayersPosition()
         if not player_positions:
             return
             
@@ -70,17 +71,41 @@ class enemy(entity, entityController):
             else:
                 self.move("MOVE_RIGHT")
     
-    # TODO: COMPLETE, ADD A ACTION QUEUE FOR THE ENEMY
-    # MAYBE STATE QUEUE INSTEAD, NEED A WAY TO POP IT WHEN THE PLAYER HAS MOVED TO A NEW POSITION
-    def avoid_obstacles(self, world):
-        pass
-        # enemy_x, enemy_y = self.xy
+    # Avoid obstacles by checking the surrounding tiles and trying to move in a different direction if the path towards the player is blocked.
+    def avoid_obstacles(self):
+        enemy_x, enemy_y = self.xy
+        opposites = {
+            "MOVE_UP": "MOVE_DOWN", 
+            "MOVE_DOWN": "MOVE_UP",
+            "MOVE_LEFT": "MOVE_RIGHT", 
+            "MOVE_RIGHT": "MOVE_LEFT"
+        }
 
-        # for 
-        # if self.world.CheckTile()
+        directions = {
+            "MOVE_UP": (enemy_x - 1, enemy_y),
+            "MOVE_DOWN": (enemy_x + 1, enemy_y),
+            "MOVE_LEFT": (enemy_x, enemy_y - 1),
+            "MOVE_RIGHT": (enemy_x, enemy_y + 1)
+        }
+
+        # Try all passable directions except the opposite of the last move
+        for dir, (nx, ny) in directions.items():
+            if dir == opposites.get(self.last_direction):
+                continue
+            if self.world.CheckTile(nx, ny) in [("empty", 0), ("player", 1)]:
+                if self.move(dir):
+                    self.last_direction = dir
+                    return
+
+        # If stuck, allow backtracking as a last resort
+        backward = opposites.get(self.last_direction)
+        if backward and self.move(backward):
+            self.last_direction = backward
+
+                
     
     # Randomly move the entity in one of the possible directions
-    def random_move(self, world):
+    def random_move(self):
         chosen = rand.choice(self.possible_actions)
         self.move(chosen)
 
